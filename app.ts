dd8cf789f91aed5a80b9fe1dbb9ee35b696ab25b
alvaro-78 <alvaro78an@gmail.com>
2021-12-09 20:22:03 +0100

    Add express config

--- app.ts
+++ app.ts
@@ -1,3 +1,8 @@
-export const name = 'Bastet';
+import dotenv from 'dotenv';
+import Server from './models/server';
 
-console.log(name);
+dotenv.config();
+
+const server = new Server();
+
+server.listen();
