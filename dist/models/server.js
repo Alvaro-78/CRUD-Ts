1fecf52aecd8f13f731d1c7385792486b8f39571
alvaro-78 <alvaro78an@gmail.com>
2021-12-10 09:38:07 +0100

    Added routes to server

--- dist/models/server.js
+++ dist/models/server.js
@@ -4,10 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const userRouter_1 = __importDefault(require("../routes/userRouter"));
 class Server {
     constructor() {
+        this.apiPaths = {
+            users: 'api/users',
+        };
         this.app = (0, express_1.default)();
         this.port = process.env.PORT || '8000';
+    }
+    routes() {
+        this.app.use(this.apiPaths.users, userRouter_1.default);
     }
     listen() {
         this.app.listen(this.port, () => {
--- models/server.ts
+++ models/server.ts
@@ -1,12 +1,20 @@
 import express, { Application } from 'express';
+import userRoutes from '../routes/userRouter';
 
 class Server {
 	private app: Application;
 	private port: string;
+	private apiPaths = {
+		users: 'api/users',
+	};
 
 	constructor() {
 		this.app = express();
 		this.port = process.env.PORT || '8000';
+	}
+
+	routes() {
+		this.app.use(this.apiPaths.users, userRoutes);
 	}
 
 	listen() {
